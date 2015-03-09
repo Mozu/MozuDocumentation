5a556d72af6082c6a4236925571970eede852315
ian_alexander <ian_alexander@volusion.com>
2015-03-09 14:49:28 -0500

    adding document creation json

---
+++ CodeSamples/Custom Content Types/POST_document.js
@@ -0,0 +1,14 @@
+{
+    "id": "summerpromotion",
+    "name": "Summer Deals",
+    "extension": ".html",
+    "documentTypeFQN": "staticPage@a633157",
+    "listFQN": "staticPages@a633157",
+    "contentLength": "1000",
+    "publishState": "Active",
+    "properties":     {
+
+    },
+    "insertDate": "/Date(-62135575200000-0600)/",
+    "updateDate": "/Date(-62135575200000-0600)/"
+}
